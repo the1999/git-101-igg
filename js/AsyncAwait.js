632a3c8eb20479db8334dd76c1deb9d6e53c5e09
the1999 <71362543+the1999@users.noreply.github.com>
2020-09-16 15:54:13 +0700

    Create AsyncAwait.js

---
+++ js/AsyncAwait.js
@@ -0,0 +1,14 @@
+myFirstAsync = () => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve('Success');
+        }, 250);
+    })
+}
+
+msg = async () => {
+    const successMessage = await myFirstAsync();
+    console.log('Yay! ' , successMessage);
+}
+
+msg();
