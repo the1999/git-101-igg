6c2a3d1cfac4e4cb52e432b83c95ee4ed6a7182b
the1999 <71362543+the1999@users.noreply.github.com>
2020-09-16 15:22:11 +0700

    quiz3

---
+++ js/quiz3.js
@@ -0,0 +1,16 @@
+if(Score >= 81){
+        Grade = "A";
+    }
+else if(Score >= 71){
+        Grade = "B";
+    }
+else if(Score >= 61){
+        Grade = "C";
+    }
+else if(Score >= 51){
+        Grade = "D";
+    }
+else{
+        Grade = "F";
+    }
+return Grade
