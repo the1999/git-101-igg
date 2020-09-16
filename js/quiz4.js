7ee17ad76e94054175bb3944435b45a1b53c2f5d
the1999 <71362543+the1999@users.noreply.github.com>
2020-09-16 15:39:54 +0700

    quiz4

---
+++ js/quiz4.js
@@ -0,0 +1,14 @@
+//หาจำนวนตัวหนังสือ
+var txt = "Hello my name is Joke";
+var sln = txt.length;
+console.log(sln)
+
+//หาว่าjokeอยู่ตัวที่เท่าไหร่
+var str = "Hello my name is Joke nice to meet you";
+var pos = str.indexOf("Joke");
+console.log(pos)
+
+//
+var str = "well open up your mindand see like me"
+var pos = str.search("open");
+console.log(pos)
