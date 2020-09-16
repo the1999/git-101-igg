1a30fb343c23a330b7969a653d0f34ed9021c4e7
the1999 <71362543+the1999@users.noreply.github.com>
2020-09-16 15:32:00 +0700

    Update quiz3.js

--- js/quiz3.js
+++ js/quiz3.js
@@ -1,16 +1,22 @@
-if(Score >= 81){
-        Grade = "A";
+const calculateGrade = (point) => {
+    let grade = null
+    if (point >= 81 && point <= 100){
+        grade = "A"
     }
-else if(Score >= 71){
-        Grade = "B";
+    else if(point >= 71 && point <= 80) {
+        grade = "B"
     }
-else if(Score >= 61){
-        Grade = "C";
+    else if(point >= 61 && point <= 70) {
+        grade = "C"
     }
-else if(Score >= 51){
-        Grade = "D";
+    else if(point >= 51 && point <= 60) {
+        grade = "D"
     }
-else{
-        Grade = "F";
+    else if(point >= 0 && point <= 50) {
+        grade = "F"
+    } else {
+        grade = "Point was invalid"
     }
-return Grade
+    return grade
+}
+console.log(calculateGrade(50))
