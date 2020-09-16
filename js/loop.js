ad360b3c440132ab0d53e08608c9cb97ee5e860a
the1999 <71362543+the1999@users.noreply.github.com>
2020-09-16 14:38:59 +0700

    loop

---
+++ js/loop.js
@@ -0,0 +1,15 @@
+for(let i = 1; i<=20; i+=1) {
+    console.log(i)
+}
+
+let temp = ['apple', 'banana', 'kiwi']
+//temp.forEach((element, idx )=>{
+    //console.log(idx ,element)
+//});
+
+let newValue = temp.map((element, idx) =>{
+    return `${idx+1}.${element}`
+})
+
+let filterMoreThanZero = [-1,2,3,56,6,-93,0,4].filter(el => el >= 0)
+console.log(filterMoreThanZero)
