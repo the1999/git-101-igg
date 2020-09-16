cbe69582df820785aa43dd2181b31fd2a6f09f63
the1999 <71362543+the1999@users.noreply.github.com>
2020-09-16 14:06:39 +0700

    function(add)

---
+++ js/function.js
@@ -0,0 +1,20 @@
+//function add(a,b) {
+    //return a + b
+//}
+
+//function mul(a, b){
+    //return a * b
+//}
+
+//function searchCustomer(customerName){
+
+//}
+
+//const add = (a,b) => a+b
+//console.log(add(2,3))
+
+const add = (a,b) => {
+    this.user = "JOKE"
+    return a+b
+}
+console.log(add(2, 3))
