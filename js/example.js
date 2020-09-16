11b25ec95f6d12363d2d67ed9c93fbc43e542ca2
the1999 <71362543+the1999@users.noreply.github.com>
2020-09-16 13:38:12 +0700

    datatype

---
+++ js/example.js
@@ -0,0 +1,18 @@
+//console.log("Hello World")
+//const PI = 3.14 //ประกาศตัวแปร
+//console.log("PI: ", PI) //แสดงผล
+
+//let name = "John"
+//name = "JOKE"
+//let test = "Hello"
+//name = `${test} B`
+//console.log("name: " ,name)
+
+
+let arr = ['apple', 'banana', 'kiwi']
+
+let person = {
+    name: 'JOKE',
+    age: 15
+}
+console.log(person)
